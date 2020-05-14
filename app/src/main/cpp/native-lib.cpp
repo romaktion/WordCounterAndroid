be316bcc18f9b930ca54714c79f1adfcd1eddede
Roman Krivosheyenko <romaktion@gmail.com>
2020-05-14 10:50:41 +0300

    Button

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,19 @@
 #include <jni.h>
 #include <string>
+#include "text.h"
+
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_romaktion_wordcounter_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+
+
+
+
+
+    auto utf8 = std::make_unique<text>("Hello from libiconv!", "UTF-8");
+    auto res = utf8->byte_string().c_str();
+
+    return env->NewStringUTF(res);
 }
