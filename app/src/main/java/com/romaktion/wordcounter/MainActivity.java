8886ec1547f6e5b53bbc2ad33dae1463b084f3e6
Roman <romaktion@gmail.com>
2020-05-18 21:30:56 +0300

    Update

--- app/src/main/java/com/romaktion/wordcounter/MainActivity.java
+++ app/src/main/java/com/romaktion/wordcounter/MainActivity.java
@@ -1,9 +1,25 @@
 package com.romaktion.wordcounter;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
   static {
     System.loadLibrary("native-lib");
   }
+  TextView textView;
+  String filePath;
+  InputStream inputStream;
+  FileOutputStream outputStream;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +38,18 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     // Example of a call to a native method
-    TextView tv = findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+    textView = findViewById(R.id.sample_text);
+    textView.setText(stringFromJNI());
+
+    findViewById(R.id.btn_filePicker).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+
+        Intent myFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        myFileIntent.setType("*/*");
+        startActivityForResult(myFileIntent, 10);
+      }
+    });
   }
 
   /**
@@ -27,4 +57,56 @@ public class MainActivity extends AppCompatActivity {
    * which is packaged with this application.
    */
   public native String stringFromJNI();
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == 10) {
+      if (resultCode == RESULT_OK) {
+        assert data != null;
+        Uri uri = Objects.requireNonNull(data.getData());
+
+        try {
+          inputStream = getContentResolver().openInputStream(uri);
+        } catch (FileNotFoundException e) {
+          e.printStackTrace();
+        }
+
+        //Permissions
+
+        //end Permissions
+
+        try {
+          outputStream = new FileOutputStream(Environment.getExternalStorageDirectory().getPath() + "/temp.jpg");
+        } catch (FileNotFoundException e) {
+          e.printStackTrace();
+        }
+
+        assert outputStream != null;
+        try {
+          assert inputStream != null;
+          outputStream.write(inputStream.read());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+
+        try {
+          inputStream.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        try {
+          outputStream.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+
+        filePath = uri.toString();
+
+        textView.setText(filePath);
+      }
+    }
+  }
+
+
 }
