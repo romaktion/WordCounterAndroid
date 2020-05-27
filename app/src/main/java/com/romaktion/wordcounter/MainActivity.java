41783730819b5d532aa27eab5957bf964f7adfbd
Roman Krivosheyenko <romaktion@gmail.com>
2020-05-27 08:55:23 +0300

    Read file

--- app/src/main/java/com/romaktion/wordcounter/MainActivity.java
+++ app/src/main/java/com/romaktion/wordcounter/MainActivity.java
@@ -1,24 +1,27 @@
 package com.romaktion.wordcounter;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,9 +31,12 @@ public class MainActivity extends AppCompatActivity {
     System.loadLibrary("native-lib");
   }
   TextView textView;
-  String filePath;
   InputStream inputStream;
-  FileOutputStream outputStream;
+  String IntermediateFileDir = Environment.getExternalStorageDirectory().getPath()
+          + File.separator + "WordCounter";
+  String IntermediateFilePath = Environment.getExternalStorageDirectory().getPath()
+          + File.separator + "WordCounter"
+          + File.separator + "temp.txt";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -64,49 +70,110 @@ public class MainActivity extends AppCompatActivity {
     if (requestCode == 10) {
       if (resultCode == RESULT_OK) {
         assert data != null;
-        Uri uri = Objects.requireNonNull(data.getData());
-
         try {
-          inputStream = getContentResolver().openInputStream(uri);
+          inputStream = getContentResolver().openInputStream(
+                  Objects.requireNonNull(data.getData()));
         } catch (FileNotFoundException e) {
           e.printStackTrace();
         }
 
         //Permissions
+        if (ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED) {
+          // You can use the API that requires the permission.
+          OpenFile();
+        } else if (shouldShowRequestPermissionRationale(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+          // In an educational UI, explain to the user why your app requires this
+          // permission for a specific feature to behave as expected. In this UI,
+          // include a "cancel" or "no thanks" button that allows the user to
+          // continue using your app without granting the permission.
+          final String message = "Storage permission is needed to show files count";
 
+          Snackbar.make(findViewById(R.id.content).getRootView(), message, Snackbar.LENGTH_LONG)
+                  .setAction("GRANT", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                      requestPermissions();
+                    }
+                  })
+                  .show();
+        } else {
+          // You can directly ask for the permission.
+          requestPermissions();
+        }
         //end Permissions
-
-        try {
-          outputStream = new FileOutputStream(Environment.getExternalStorageDirectory().getPath() + "/temp.jpg");
-        } catch (FileNotFoundException e) {
-          e.printStackTrace();
-        }
-
-        assert outputStream != null;
-        try {
-          assert inputStream != null;
-          outputStream.write(inputStream.read());
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-
-        try {
-          inputStream.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-        try {
-          outputStream.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-
-        filePath = uri.toString();
-
-        textView.setText(filePath);
       }
     }
   }
 
+  private void requestPermissions() {
+    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+  }
 
+  @Override
+  public void onRequestPermissionsResult(int requestCode,
+                                         @NonNull String[] permissions, @NonNull int[] grantResults) {
+    if (requestCode == 1) {
+      if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        OpenFile();
+      }
+      else {
+        Toast.makeText(MainActivity.this, "Permission denied to write your External storage", Toast.LENGTH_SHORT).show();
+      }
+    }
+  }
+
+  private void OpenFile() {
+    File file = new File(IntermediateFileDir);
+    if (!file.exists()) {
+      if (!file.mkdirs()) {
+        //TODO: Error handle
+      }
+    }
+
+    FileOutputStream outputStream = null;
+    try {
+      outputStream = new FileOutputStream(IntermediateFilePath);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    assert outputStream != null;
+    try {
+      assert inputStream != null;
+      byte[] buffer = new byte[1024];
+      int read;
+
+      while ((read = inputStream.read(buffer)) != -1)
+        outputStream.write(buffer, 0, read);
+
+      outputStream.flush();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    try {
+      inputStream.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    try {
+      outputStream.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    textView.setText(IntermediateFilePath);
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+
+    File file = new File(IntermediateFilePath);
+    if (file.exists())
+      file.delete();
+  }
 }
